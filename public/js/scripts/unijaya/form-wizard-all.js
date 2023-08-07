a3d44bb63da954746a5d9354d3e5d54f474d1a95
seqinah <110380408+seqinah@users.noreply.github.com>
2023-08-07 11:15:02 +0800

    [npm run dev] to add jata negara in public files

--- public/js/scripts/unijaya/form-wizard-all.js
+++ public/js/scripts/unijaya/form-wizard-all.js
@@ -1,1 +1,155 @@
-$((function(){"use strict";var e=document.querySelectorAll(".bs-stepper"),n=$(".select2"),t=document.querySelectorAll(".horizontal-wizard"),r=document.querySelectorAll(".vertical-wizard"),i=document.querySelectorAll(".modern-wizard"),o=document.querySelectorAll(".modern-vertical-wizard");if(void 0!==typeof e&&null!==e)for(var c=0;c<e.length;++c)e[c].addEventListener("show.bs-stepper",(function(e){for(var n=e.detail.indexStep,t=$(e.target).find(".step").length-1,r=$(e.target).find(".step"),i=0;i<n;i++){r[i].classList.add("crossed");for(var o=n;o<t;o++)r[o].classList.remove("crossed")}if(0==e.detail.to){for(var c=n;c<t;c++)r[c].classList.remove("crossed");r[0].classList.remove("crossed")}}));if(n.each((function(){var e=$(this);e.wrap('<div class="position-relative"></div>'),e.select2({placeholder:"Select value",dropdownParent:e.parent()})})),void 0!==typeof t&&null!==t){var l=[];t.forEach(((e,n)=>{l[n]=new Stepper(e),$(e).find(".btn-next").on("click",(function(e){l[n].next()})),$(e).find(".btn-prev").on("click",(function(){l[n].previous()}))}))}if(void 0!==typeof r&&null!==r){var a=[];r.forEach(((e,n)=>{a[n]=new Stepper(e,{linear:!1}),$(e).find(".btn-next").on("click",(function(){a[n].next()})),$(e).find(".btn-prev").on("click",(function(){a[n].previous()}))}))}if(void 0!==typeof i&&null!==i){var d=[];i.forEach(((e,n)=>{d[n]=new Stepper(e,{linear:!1}),$(e).find(".btn-next").on("click",(function(){d[n].next()})),$(e).find(".btn-prev").on("click",(function(){d[n].previous()}))}))}if(void 0!==typeof o&&null!==o){var f=[];o.forEach(((e,n)=>{f[n]=new Stepper(e,{linear:!1}),$(e).find(".btn-next").on("click",(function(){f[n].next()})),$(e).find(".btn-prev").on("click",(function(){f[n].previous()}))}))}}));
+/*=========================================================================================
+    File Name: form-wizard.js
+    Description: Wizard step page, universal. Make sure to have select2,bs-stepper before adding this
+    ----------------------------------------------------------------------------------------
+    Author: Unijaya - Hafiz
+    Author Contact: +6019-2334157
+==========================================================================================*/
+
+$(function () {
+    'use strict';
+
+    var bsStepper = document.querySelectorAll('.bs-stepper'),
+        select = $('.select2'),
+        horizontalWizardList = document.querySelectorAll('.horizontal-wizard'),
+        verticalWizardList = document.querySelectorAll('.vertical-wizard'),
+        modernWizardList = document.querySelectorAll('.modern-wizard'),
+        modernVerticalWizardList = document.querySelectorAll('.modern-vertical-wizard');
+
+
+    // Adds crossed class
+    if (typeof bsStepper !== undefined && bsStepper !== null) {
+        for (var el = 0; el < bsStepper.length; ++el) {
+            bsStepper[el].addEventListener('show.bs-stepper', function (event) {
+                var index = event.detail.indexStep;
+                var numberOfSteps = $(event.target).find('.step').length - 1;
+                var line = $(event.target).find('.step');
+
+                // The first for loop is for increasing the steps,
+                // the second is for turning them off when going back
+                // and the third with the if statement because the last line
+                // can't seem to turn off when I press the first item. ¯\_(ツ)_/¯
+
+                for (var i = 0; i < index; i++) {
+                    line[i].classList.add('crossed');
+
+                    for (var j = index; j < numberOfSteps; j++) {
+                        line[j].classList.remove('crossed');
+                    }
+                }
+                if (event.detail.to == 0) {
+                    for (var k = index; k < numberOfSteps; k++) {
+                        line[k].classList.remove('crossed');
+                    }
+                    line[0].classList.remove('crossed');
+                }
+            });
+        }
+    }
+
+    // select2
+    select.each(function () {
+        var $this = $(this);
+        $this.wrap('<div class="position-relative"></div>');
+        $this.select2({
+        placeholder: 'Select value',
+        dropdownParent: $this.parent()
+        });
+    });
+
+    //Horizontal Wizard All
+    // --------------------------------------------------------------------
+    if (typeof horizontalWizardList !== undefined && horizontalWizardList !== null) {
+
+        var numberedStepperList = [];
+        horizontalWizardList.forEach((horizontalWizard,index) => {
+            numberedStepperList[index] = new Stepper(horizontalWizard);
+
+            $(horizontalWizard)
+            .find('.btn-next')
+            .on('click', function (e) {
+                numberedStepperList[index].next();
+            });
+
+            $(horizontalWizard)
+            .find('.btn-prev')
+            .on('click', function () {
+                numberedStepperList[index].previous();
+            });
+        });
+    }
+
+    //Vertical Wizard All
+    // --------------------------------------------------------------------
+    if (typeof verticalWizardList !== undefined && verticalWizardList !== null) {
+        var verticalStepperList = [];
+        verticalWizardList.forEach((verticalWizard,index) => {
+
+            verticalStepperList[index] = new Stepper(verticalWizard, {
+                linear: false
+            });
+
+            $(verticalWizard)
+            .find('.btn-next')
+            .on('click', function () {
+                verticalStepperList[index].next();
+            });
+
+            $(verticalWizard)
+            .find('.btn-prev')
+            .on('click', function () {
+                verticalStepperList[index].previous();
+            });
+        });
+    }
+
+    // Modern Wizard All
+    // --------------------------------------------------------------------
+    if (typeof modernWizardList !== undefined && modernWizardList !== null) {
+
+        var modernStepperList = [];
+        modernWizardList.forEach((modernWizard,index) => {
+
+            modernStepperList[index] = new Stepper(modernWizard, {
+                linear: false
+            });
+
+            $(modernWizard)
+            .find('.btn-next')
+            .on('click', function () {
+                modernStepperList[index].next();
+            });
+
+            $(modernWizard)
+            .find('.btn-prev')
+            .on('click', function () {
+                modernStepperList[index].previous();
+            });
+
+        });
+    }
+
+    // Modern Vertical Wizard All
+    // --------------------------------------------------------------------
+    if (typeof modernVerticalWizardList !== undefined && modernVerticalWizardList !== null) {
+
+        var modernVerticalStepperList = [];
+        modernVerticalWizardList.forEach((modernVerticalWizard,index) => {
+            modernVerticalStepperList[index] = new Stepper(modernVerticalWizard, {
+                linear: false
+            });
+
+            $(modernVerticalWizard)
+            .find('.btn-next')
+            .on('click', function () {
+                modernVerticalStepperList[index].next();
+            });
+
+            $(modernVerticalWizard)
+            .find('.btn-prev')
+            .on('click', function () {
+                modernVerticalStepperList[index].previous();
+            });
+        });
+    }
+});
