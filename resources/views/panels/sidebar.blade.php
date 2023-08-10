56badd4adf5be0e56b99362e5f7d98f7cc699625
AhYew <leewengyew.unijaya@gmail.com>
2023-08-10 16:01:56 +0800

    [composer install] Helpdesk

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -195,6 +195,14 @@ $configData = Helper::applClasses();
                                 <span class="menu-title text-truncate"> Senarai </span>
                             </a>
                         </li>
+                    @hasanyrole('superadmin')
+                        <li class="{{ in_array(request()->route()->getName(), ['helpdesk.categoryList']) ? 'active' : '' }}" >
+                            <a href="{{ route('helpdesk.categoryList') }}" class="nav-link">
+                                <i class="fa-solid fa-gear"></i>
+                                <span class="menu-title text-truncate"> Pengurusan </span>
+                            </a>
+                        </li>
+                    @endhasanyrole
                     </ul>
                 </li>
             @endif --}}
