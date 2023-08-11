6b50ba2589dab9fd7fc4d458eaaba6cdf8f3fe80
seqinah <110380408+seqinah@users.noreply.github.com>
2023-08-11 18:39:03 +0800

    [Maklumat Pemohon] Designed UI

--- resources/views/maklumat_pemohon/carian_pemohon.blade.php
+++ resources/views/maklumat_pemohon/carian_pemohon.blade.php
@@ -10,24 +10,158 @@ Maklumat Pemohon
 @endsection
 
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <h4 class="fw-bolder">Carian Maklumat Pemohon</h4>
-    </div>
+<section id="faq-search-filter">
+    <div class="card faq-search" style="background-image: url('{{asset('images/banner/banner.png')}}')">
+        <div class="card-body text-center">
+            <h2 class="text-primary">Carian Maklumat Pemohon</h2>
+            <p class="card-text mb-2">Isikan no. kad pengenalan calon dan tekan butang Cari</p>
 
-    <hr>
+            <form class="faq-search-input"> {{-- PLEASE DON'T CHANGE THE FORM CLASS --}}
+                <div class="input-group input-group-merge">
+                    <div class="input-group-text">
+                        <i data-feather="search"></i>
+                    </div>
 
-    <div class="card-body">
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-lg-6">
-                <label class="fw-bolder mb-1">No. Kad Pengenalan Calon:</label>
-                <div class="input-group">
-                    <input type="text" class="form-control"
-                        placeholder="Isikan no. kad pengenalan calon dan tekan butang Cari">
+                    {{-- Search form --}}
+                    <input type="text" class="form-control" placeholder="No. Kad Pegenalan Calon" />
                     <button class="btn btn-primary waves-effect" type="button">
                         Cari
                     </button>
                 </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<hr>
+
+<div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-12 order-2 order-lg-1">
+        <div class="card">
+            <div class="card-body">
+                <p class="card-title fw-bolder">Maklumat Permohonan</p>
+                <hr>
+
+                {{-- Maklumat Pemohon [Nama dan No KP] --}}
+                <div class="mt-2">
+                    <h5 class="fw-bolder">Nama Pemohon:</h5>
+                    <p class="card-text">
+                        Princess Aura Nurr Ermily Amara Auliya Bidadari Nawal El Zendra binti Mohd Suffian {{-- NAMA PEMOHON --}}
+                    </p>
+                </div>
+                <div class="mt-2">
+                    <h5 class="fw-bolder">No Kad Pengenalan:</h5>
+                    <p class="card-text">
+                        990701010011 {{-- IC PEMOHON --}}
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <p class="card-title fw-bolder">Garis Masa Permohonan</p>
+                <hr>
+
+                {{-- TIMELINE PERMOHONAN --}}
+                <ul class="timeline mt-2">
+                    {{-- This is where looping started if needed --}}
+                    <li class="timeline-item">
+                        <span class="timeline-point timeline-point-indicator"></span>
+                        <div class="timeline-event">
+                            <div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">
+                                <h6>Permohonan Dikemaskini</h6> {{-- Timeline Activity --}}
+                                <span class="timeline-event-time">12 min lalu</span> {{-- Timeline Time --}}
+                            </div>
+                        </div>
+                    </li>
+                    {{-- End of looping --}}
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-9 col-12 order-1 order-lg-2">
+        <div class="card">
+            <div class="card-body">
+                <nav class="navbar navbar-expand-md navbar-light justify-content-end justify-content-md-between w-100 mx-1">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div class="profile-tabs d-flex justify-content-between flex-wrap">
+                            <ul class="nav nav-pills" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold active" id="peribadi-tab" data-bs-toggle="tab"
+                                        href="#peribadi" aria-controls="peribadi" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Maklumat Peribadi</span>
+                                        <i data-feather="rss" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold" id="skim-tab" data-bs-toggle="tab" href="#skim"
+                                        aria-controls="skim" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Maklumat Skim</span>
+                                        <i data-feather="info" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold" id="akademik-tab" data-bs-toggle="tab" href="#akademik"
+                                        aria-controls="akademik" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Makluamt Akademik</span>
+                                        <i data-feather="image" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold" id="perkhidmatan-tab" data-bs-toggle="tab" href="#perkhidmatan"
+                                        aria-controls="perkhidmatan" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Perkhidmatan Pegawai</span>
+                                        <i data-feather="users" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold" id="tambahan-tab" data-bs-toggle="tab" href="#tambahan"
+                                        aria-controls="tambahan" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Maklumat Tambahan</span>
+                                        <i data-feather="users" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link fw-bold" id="tatatertib-tab" data-bs-toggle="tab" href="#tatatertib"
+                                        aria-controls="tatatertib" role="tab" aria-selected="true">
+                                        <span class="d-none d-md-block">Tatatertib</span>
+                                        <i data-feather="users" class="d-block d-md-none"></i>
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <div class="tab-content">
+                                <div class="tab-pane fade show active" id="peribadi" role="tabpanel" aria-labelledby="peribadi-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.maklumat_peribadi')
+                                </div>
+                                <div class="tab-pane fade" id="skim" role="tabpanel" aria-labelledby="skim-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.maklumat_skim')
+                                </div>
+                                <div class="tab-pane fade" id="akademik" role="tabpanel" aria-labelledby="akademik-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.maklumat_akademik')
+                                </div>
+                                <div class="tab-pane fade" id="perkhidmatan" role="tabpanel" aria-labelledby="perkhidmatan-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.perkhidmatan')
+                                </div>
+                                <div class="tab-pane fade" id="tambahan" role="tabpanel" aria-labelledby="tambahan-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.maklumat_tambahan')
+                                </div>
+                                <div class="tab-pane fade" id="tatatertib" role="tabpanel" aria-labelledby="tatatertib-tab">
+                                    <hr>
+                                    @include('maklumat_pemohon.pemohon.tatatertib')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </div>
     </div>
--- resources/views/maklumat_pemohon/pemohon/maklumat_peribadi.blade.php
+++ resources/views/maklumat_pemohon/pemohon/maklumat_peribadi.blade.php
@@ -1,5 +1,158 @@
-<div class="card">
-    <div class="card-header">
-        <h4 class="fw-bolder">Maklumat Peribadi</h4>
+<div class="row">
+    {{-- MAKLUMAT PERIBADI --}}
+    <h6 class="fw-bolder mb-1">Maklumat Peribadi</h6>
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Jantina</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Agama</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Keturunan</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Tarikh Lahir</label>
+        <input type="text" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" value="" disabled />
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Umur</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Taraf Perkahwinan</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">No. Telefon</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">Alamat Emel</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <hr>
+
+    {{-- ALAMAT --}}
+    <h6 class="fw-bolder mb-1">Alamat</h6>
+
+    {{-- ALAMAT TETAP --}}
+    <h6>
+        <span class="badge badge-light-primary fw-bolder">Alamat Tetap</span>
+    </h6>
+    <div class="col-sm-12 col-md-12 col-lg-12 mb-1">
+        <label class="form-label">Alamat</label>
+        <textarea rows="3" type="text" class="form-control" value="" disabled></textarea>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Poskod</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Bandar</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Negeri</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    {{-- ALAMAT SURAT MENYURAT --}}
+    <h6>
+        <span class="badge badge-light-primary fw-bolder mt-1">Alamat Surat Menyurat</span>
+    </h6>
+    <div class="col-sm-12 col-md-12 col-lg-12 mb-1">
+        <label class="form-label">Alamat</label>
+        <textarea rows="3" type="text" class="form-control" value="" disabled></textarea>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Poskod</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Bandar</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Negeri</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+    
+    <hr>
+
+    {{-- TEMPAT LAHIR --}}
+    <h6 class="fw-bolder mb-1">Tempat Lahir</h6>
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Pemohon</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Ibu Pemohon</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Bapa Pemohon</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <hr>
+
+    {{-- LESEN MEMANDU --}}
+    <h6 class="fw-bolder mb-1">Lesen Memandu</h6>
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Jenis Lesen</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Tarikh Tamat</label>
+        <input type="text" class="form-control flatpickr-basic" placeholder="YYYY-MM-DD" value="" disabled />
+    </div>
+
+    <div class="col-sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label">Status Senarai Hitam</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <hr>
+
+    {{-- KURANG UPAYA (OKU) --}}
+    <h6 class="fw-bolder mb-1">Kurang Upaya (OKU)</h6>
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">No. Pendaftaran OKU</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">Status OKU</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">Kategori OKU</label>
+        <input type="text" class="form-control" value="" disabled>
+    </div>
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mb-1">
+        <label class="form-label">Sub- Kategori OKU</label>
+        <input type="text" class="form-control" value="" disabled>
     </div>
 </div>
