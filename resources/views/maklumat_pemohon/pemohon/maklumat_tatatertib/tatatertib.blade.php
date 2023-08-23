8a10017508f585961333656983b653c89b4a6305
seqinah <110380408+seqinah@users.noreply.github.com>
2023-08-23 17:29:08 +0800

    [maklumat tatatertib]

--- resources/views/maklumat_pemohon/pemohon/maklumat_tatatertib/tatatertib.blade.php
+++ resources/views/maklumat_pemohon/pemohon/maklumat_tatatertib/tatatertib.blade.php
@@ -1,4 +1,49 @@
 <div class="row mt-2 mb-2">
+
+    <div class="col sm-12 col-md-12 col-lg-12 mb-1">
+        <label class="form-label fw-bolder">Tindakan Tatatertib</label>
+        <select class="form-select select2" name="" id="">
+            <option value="" hidden>Tindakan Tatatertib</option>
+        </select>
+    </div>
+
+    <div class="col sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label fw-bolder">Tempoh Hukuman</label>
+            <div class="input-group">
+                <input type="text" class="form-control">
+                <span class="input-group-text" id="basic-addon-search1">
+                    <select class="form-control select2" name="jenis" id="jenis">
+                        <option value="" hidden>Spesis</option>
+                        <option value="Ayam">Tahun</option>
+                        <option value="Babi">Bulan</option>
+                        <option value="Kerbau">Hari</option>
+                    </select>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label fw-bolder">Tarikh Mula Hukuman</label>
+        <input type="date" class="form-control" name="" id="">
+    </div>
+
+    <div class="col sm-4 col-md-4 col-lg-4 mb-1">
+        <label class="form-label fw-bolder">Tarikh Akhir Hukuman</label>
+        <input type="date" class="form-control" name="" id="" disabled>
+    </div>
+
+    <div class="d-flex justify-content-end align-items-center my-1 ">
+        <a class="me-3" type="button" id="reset" href="#">
+            <span class="text-danger"> Set Semula </span>
+        </a>
+        <button type="submit" class="btn btn-success float-right">
+            <i class="fa fa-save"></i> Simpan
+        </button>
+    </div>
+
+    <hr>
+
     <div class="table-responsive">
         <table class="table header_uppercase table-bordered table-hovered">
             <thead>
