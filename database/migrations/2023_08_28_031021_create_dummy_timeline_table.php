ae0907de30ea6c2c1edd6032b42e0dff2a1cf995
Faiz Roslee <faizroslee.unijaya@gmail.com>
2023-08-28 03:36:26 +0800

    Pemohonan Timeline

---
+++ database/migrations/2023_08_28_031021_create_dummy_timeline_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dummy_timeline', function (Blueprint $table) {
+            $table->id();
+            $table->string('dummy_penalty_id');
+            $table->string('details');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->comment('Last Update User')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dummy_timeline');
+    }
+};
