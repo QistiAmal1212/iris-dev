384d2018adec92b05eb591a5b1c7223787ce2557
AhmadMujahid <ahmadmujahid.unijaya@gmail.com>
2023-10-03 21:53:20 +0800

    Tambah table rujukan

---
+++ database/migrations/2023_10_03_214058_create_ruj_kumpulan_jkk_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruj_kumpulan_jkk', function (Blueprint $table) {
+            $table->id();
+            $table->string('kod')->unique();
+            $table->string('nama');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->comment('Last Update User')->nullable();
+            $table->timestamps();
+            $table->boolean('sah_yt')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruj_kumpulan_jkk');
+    }
+};
