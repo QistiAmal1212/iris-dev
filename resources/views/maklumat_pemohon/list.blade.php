cd90022f6994b9b06f3af67c7ae4e4a7d36401cc
hafizruslan <145441557+hafizruslan@users.noreply.github.com>
2023-10-07 09:56:24 +0530

    update name search function

---
+++ resources/views/maklumat_pemohon/list.blade.php
@@ -0,0 +1,52 @@
+<div class="btn-group" style="float: right;">
+  <button type="button" class="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Page {{$currentPage}}
+  </button>
+  <ul class="dropdown-menu overflow-auto" style="height: 300px;float: right;">
+  	@for($i=1; $i <= $total_pages; $i++)
+    	<li><a class="dropdown-item" onclick="updatePage({{$i}},{{$total_pages}})">{{$i}}</a></li>
+   	@endfor
+  </ul>
+</div>
+<br>
+<br>
+<br>
+<table class="table header_uppercase table-bordered" id="table-carian">
+<thead>
+<tr>
+	<th width="10%">#</th>
+	<th>No Kad Pengenalan Baru</th>
+	<th width="55%">Nama Penuh</th>
+	<th width="15%">Lihat Maklumat</th>
+	</tr>
+</thead>
+<tbody>
+@foreach($candidate as $key => $user)
+<?php
+	if(!isset($user->no_kp_baru)) {
+		continue;
+	}
+	$index = $key+1;
+	if ($currentPage > 1) 
+		$index = $index + ($currentPage*10);
+?>
+<tr>
+	<td>{{$index}}</td>
+	<td>{{ $user->no_kp_baru }}</td>
+	<td>{{ $user->nama_penuh }}</td>
+	<td><div class="btn-group btn-group-sm d-flex justify-content-center" role="group" aria-label="Action"><a class='btn btn-xs btn-default' onclick="searchCandidate({{$user->no_kp_baru}})"> <i class="fas fa-pencil text-primary"></i></div> </td>
+</tr>
+@endforeach
+</tbody>
+</table>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item">
+      <a class="page-link" onclick="updatePage({{ $previousPage }}, {{$total_pages}})">Previous</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" onclick="updatePage({{ $nextPage }}, {{$total_pages}})">Next</a>
+    </li>
+  </ul>
+</nav>
