8151126cbbaaa4792d2580737874f0e3be065c20
AhmadMujahid <ahmadmujahid.unijaya@gmail.com>
2023-10-09 09:34:17 +0800

    cronjob check user activity daily

---
+++ database/migrations/2023_10_09_091726_create_ruj_skim_jkkc_sijil_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruj_skim_jkkc_sijil', function (Blueprint $table) {
+            $table->id();
+            $table->string('kod')->unique();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->comment('Last Update User')->nullable();
+            $table->timestamps();
+            $table->string('sah_yt')->default('Y');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruj_skim_jkkc_sijil');
+    }
+};
