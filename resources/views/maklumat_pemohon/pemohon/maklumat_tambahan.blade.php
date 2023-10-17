c7738e3f3284064c749969eb6c0b099cded0c9fc
seqinah <110380408+seqinah@users.noreply.github.com>
2023-10-17 11:33:54 +0800

    [Carian pemohon] Changed color of sub-section

--- resources/views/maklumat_pemohon/pemohon/maklumat_tambahan.blade.php
+++ resources/views/maklumat_pemohon/pemohon/maklumat_tambahan.blade.php
@@ -1,5 +1,5 @@
 <div class="">
-    <ul class="nav nav-pills nav-justified" role="tablist">
+    <ul class="nav nav-pills nav-pill-info nav-justified" role="tablist">
         <li class="nav-item" role="presentation">
             <a class="nav-link fw-bolder active" id="tentera-tab" data-bs-toggle="tab" href="#tentera" aria-controls="tentera" role="tab" aria-selected="true">
                 Maklumat Bekas Tentera
