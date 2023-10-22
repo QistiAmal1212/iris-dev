71ff3489aff788a4a2e6bb4f0941306bd1574bb8
hafizruslan <145441557+hafizruslan@users.noreply.github.com>
2023-10-22 13:00:44 +0530

    add oku in log

--- app/Http/Controllers/MaklumatPemohonController.php
+++ app/Http/Controllers/MaklumatPemohonController.php
@@ -670,6 +670,7 @@ class MaklumatPemohonController extends Controller
                 'activity_type_id' => 4,
                 'created_by' => auth()->user()->id,
                 'updated_by' => auth()->user()->id,
+                'tukar_log' => isset($request->tukar_log_oku) ? json_encode($request->tukar_log_oku) : null
             ]);
 
             DB::commit();
